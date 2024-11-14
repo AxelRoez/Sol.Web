a579a662b82f17e6f27bc5d839e166d20a70257c
Uldethose <Uldethose>
2024-11-14 00:02:08 -0500

    Se agrego  Ofertas en el header falta completar las muestras

---
+++ src/app/components/auth/ofertas/oferta.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core'
+
+
+
+@Component ({
+
+    selector: 'app-ofertas',
+    templateUrl: './oferta.component.html',
+    styleUrls: ['./oferta.component.css']
+})
+
+export class OfertaComponent{
+
+}
